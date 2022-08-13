39272935bd50f85f9598a41634dee593ed4c3c4e
DianaB177 <anaidb77@gmail.com>
2022-08-13 23:19:30 +0200

    background error

--- weather/src/Componets/BackgroundDay.js
+++ weather/src/Componets/BackgroundDay.js
@@ -5,13 +5,14 @@ import DayNightToggle from 'react-day-and-night-toggle'
 
 const BackgroundDay = () => {
   const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('data-theme') === 'dark' ? true : false)
-
+ /*let swichtbackground = document.querySelector('#weather');*/
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
     const newColorScheme = e.matches ? 'dark' : 'light'
 
     setIsDarkMode(newColorScheme === 'dark' ? true : false)
     localStorage.setItem('data-theme', newColorScheme)
     document.body.setAttribute('data-theme', localStorage.getItem('data-theme'))
+    /*swichtbackground.setAttribute('data-theme', localStorage.getItem('data-theme'))*/
   })
 
   const handleChangeTheme = () => {
@@ -19,16 +20,18 @@ const BackgroundDay = () => {
     if(!isDarkMode) {
       localStorage.setItem('data-theme', 'dark')
       document.body.setAttribute('data-theme', 'dark')
+      /*swichtbackground.setAttribute('data-theme', 'dark')*/
     } else {
       localStorage.setItem('data-theme', 'light')
       document.body.setAttribute('data-theme', 'light')
+      /*swichtbackground.setAttribute('data-theme', 'light')*/
     }
   }
 
   return (
     <div className="App">
       <div className="swichtDay">
-        <DayNightToggle size={100} onChange={handleChangeTheme} checked={isDarkMode} />
+        <DayNightToggle size={50} onChange={handleChangeTheme} checked={isDarkMode} />
       </div>
     </div>
   )
